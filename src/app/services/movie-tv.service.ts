6ff00b5c4538c6c786790b450a8d70da36e28a4b
SophearithSaing <45601533+SophearithSaing@users.noreply.github.com>
2021-05-12 23:49:39 +0700

    movie service

--- src/app/services/movie-tv.service.ts
+++ src/app/services/movie-tv.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Movie } from '../models/movie';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieTvService {
 
-  constructor() { }
+  apiUrl = environment.apiUrl;
+  apiImageUrl = environment.apiImageUrl;
+  apiKey = environment.apiKey;
+
+  constructor(private http: HttpClient) { }
+
+  getTrendingMovies = () => {
+    const url = `${this.apiUrl}/trending/movie/week?api_key=${this.apiKey}`;
+    return this.http.get<Movie>(url);
+  };
+
+  getPopularMovies = () => {
+    const url = `${this.apiUrl}/movie/popular?api_key=${this.apiKey}`;
+    return this.http.get<Movie>(url);
+  };
+
+  getOneMovie = (id: number) => {
+    const url = `${this.apiUrl}/movie/${id}?api_key=${this.apiKey}`;
+    return this.http.get<Movie>(url);
+  };
+
+  searchMovies = (query: string) => {
+    const url = `${this.apiUrl}/search/movie?api_key=${this.apiKey}&query=${query}`;
+    return this.http.get<{ results: Movie[] }>(url);
+  };
+
+  getImage = (path: string, width = '/original') => {
+    if (width !== '/original') {
+      width = `/w${width}`;
+    }
+    return `${this.apiImageUrl}${width}${path}`;
+  };
+
+  formatDate = (date: string) => {
+    const oldDate = new Date(date);
+    const str = oldDate.toDateString().split(' ');
+    return `${str[0]}, ${str[2]} ${str[1]} ${str[3]}`;
+  };
+
+  saveMovie = () => {
+
+  };
+
+  saveTV = () => {
+
+  };
 }
